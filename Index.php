69a2e9c9599995f41f6aa3bd73bfcf7110e7a169
jyoti-wp <sim_s006@icloud.com>
2020-12-09 21:47:49 +0530

    learning wordpress

---
+++ Index.php
@@ -0,0 +1,11 @@
+<?php 
+    function greet($name, $color) {
+       echo "<P>Hi, My name is $name and my favourite color is $color</P>";
+    }
+    greet('John', 'Blue');
+    greet('Jane', 'Green ');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
+
