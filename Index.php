eabe26e4e1b09ea7f56f3e330fd00feeaa7e61a2
jyoti-wp <sim_s006@icloud.com>
2020-12-10 15:18:19 +0530

    wordpress theme basic update

--- Index.php
+++ Index.php
@@ -1,11 +1,12 @@
 <?php 
-    function greet($name, $color) {
-       echo "<P>Hi, My name is $name and my favourite color is $color</P>";
+    function myWebsite($name, $number, $location){
+        echo "<p>Hi my name is $name and my phone number is $number followed by the location $location</p>";
+
+
     }
-    greet('John', 'Blue');
-    greet('Jane', 'Green ');
+
+    myWebsite('Jyoti', 564826743587, 'Pune');
+    myWebsite('Priya', 564671548716, 'Delhi');
+    myWebsite('Rucha', 578645986586, 'Nasik');
+    myWebsite('Avnish', 548275492675, 'Lucknow');
 ?>
-
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
-
