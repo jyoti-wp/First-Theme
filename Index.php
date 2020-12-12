514f3a2db8f9dade6e922bc11da228e322798b69
jyoti-wp <sim_s006@icloud.com>
2020-12-12 10:39:46 +0530

    learning php and function

--- Index.php
+++ Index.php
@@ -1,12 +1,4 @@
 <?php 
-    function myWebsite($name, $number, $location){
-        echo "<p>Hi my name is $name and my phone number is $number followed by the location $location</p>";
-
-
-    }
-
-    myWebsite('Jyoti', 564826743587, 'Pune');
-    myWebsite('Priya', 564671548716, 'Delhi');
-    myWebsite('Rucha', 578645986586, 'Nasik');
-    myWebsite('Avnish', 548275492675, 'Lucknow');
+   
 ?>
+
