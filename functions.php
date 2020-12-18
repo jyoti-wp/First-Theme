48bda190626c3f8dc28e302c61dc8b7c1e3ad685
jyoti-wp <sim_s006@icloud.com>
2020-12-18 23:04:34 +0530

    index php file update

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
 
 function first_theme() {
+    wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('first_theme_main_style', get_stylesheet_uri());
 }
 
