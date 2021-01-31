5448552f1a644df4af0e8052f2b304bacef74b10
jyoti-wp <sim_s006@icloud.com>
2021-01-31 09:01:06 +0530

    blog page update

---
+++ archive.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+    <div class="page-banner__content container t-center c-white">
+        <h1 class="headline headline--large"><?php if (is_category()) {
+            echo "the name will go here";
+        } 
+        if (is_author()) {
+            echo 'author name will go here';
+        }?></h1>
+        <h2 class="headline headline--medium">Again some Dummy text here.</h2>
+        <a href="#" class="btn btn--large btn--blue">Find Your Interests  </a>
+    </div>
+  </div>
+
+  <div class="container container--narrow page-section">
+    <?php 
+      while (have_posts()) {
+        the_post(); ?>
+
+          <div class="post-item">
+            <h2 class="headline headline--medium headline--post-title" ><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <div class="metabox">
+              <p>Post By <?php the_author_posts_link();  ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+            </div>
+            <div class="generic-content">
+              <?php the_excerpt(); ?>
+              <p>
+              <a class="btn btn--gray" href="<?php the_permalink(); ?>">continue Reading &raquo;</a>
+              </p>
+            </div>
+          </div>
+
+      <?php }
+      echo paginate_links();
+    ?>
+  </div>
+
+  hello
+<?php get_footer(); ?>
